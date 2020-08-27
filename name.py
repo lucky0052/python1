86e133a82c9f5adb868718485c2cba2a625084c2
Lucky0052 <lucky52.bhaman@gmail.com>
2020-08-27 18:03:48 +0530

    Added New file name.py

---
+++ name.py
@@ -0,0 +1,25 @@
+import random
+class name:
+    guess = random.randrange(1,10)
+    def __init__(self,name):
+        self.name = name
+
+    @property
+    def id(self):
+        return self.ids
+
+    @id.setter
+    def id(self,ids):
+        self.ids = ids
+        return self.ids
+
+
+    def display(self):
+        return f'{self.name}{name.guess}{self.id}'
+
+stu1 = name('Lucky')
+stu1.id = "53"
+print(stu1.display())
+
+
+# reg = name + ""
